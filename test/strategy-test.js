435b97c2df4623434be843f03c6fefbbd412ee86
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-04-13 18:27:13 +0300

    Start servers in tests

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -1,7 +1,27 @@
 var should       = require('chai').Should(),
-    LdapStrategy = require('passport-ldapauth').Strategy;
+    LdapStrategy = require('passport-ldapauth').Strategy,
+    ldapserver   = require('./ldapserver'),
+    appserver    = require('./appserver');
+
+LDAP_PORT = 1389;
 
 describe("LDAP authentication strategy", function() {
+  before(function(cb) {
+    ldapserver.start(LDAP_PORT, function() {
+      appserver.start(LDAP_PORT, function() {
+        cb();
+      });
+    });
+  });
+
+  after(function(cb) {
+    appserver.close(function() {
+      ldapserver.close(function() {
+        cb();
+      });
+    });
+  });
+
   it("should throw an error if verify callback is not provided", function(cb) {
     (function() {
       new LdapStrategy();
