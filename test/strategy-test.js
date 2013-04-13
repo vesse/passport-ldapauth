3dfa09c42745c344c3df70447aaed08f2b1b259d
Vesa Poikajärvi <vesa.poikajarvi@leonidasoy.fi>
2013-04-13 19:00:50 +0300

    First login test and authentication implementation
    First authentication implementation. LDAP test server does not work yet.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -1,14 +1,18 @@
 var should       = require('chai').Should(),
     LdapStrategy = require('passport-ldapauth').Strategy,
+    request      = require('supertest'),
     ldapserver   = require('./ldapserver'),
     appserver    = require('./appserver');
 
 LDAP_PORT = 1389;
 
 describe("LDAP authentication strategy", function() {
+  var expressapp = null;
+
   before(function(cb) {
     ldapserver.start(LDAP_PORT, function() {
-      appserver.start(LDAP_PORT, function() {
+      appserver.start(LDAP_PORT, function(app) {
+        expressapp = app;
         cb();
       });
     });
@@ -41,5 +45,21 @@ describe("LDAP authentication strategy", function() {
       new LdapStrategy({}, function() {});
     }).should.throw(Error);
     cb();
-  })
+  });
+
+  it("should return unauthorized if credentials are not given", function(cb) {
+    request(expressapp)
+      .post('/login')
+      .send({})
+      .expect(401)
+      .end(cb);
+  });
+
+  it("should allow access with valid credentials", function(cb) {
+    request(expressapp)
+      .post('/login')
+      .send({username: 'valid', password: 'valid'})
+      .expect(200)
+      .end(cb);
+  });
 });
